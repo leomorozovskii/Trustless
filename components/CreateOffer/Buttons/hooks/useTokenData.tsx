81d454faaeb2f036ddcfa5f9bf44dc29103e7e93
Misha Yakubchuk <misha.yakubchuk@metalamp.io>
2024-06-14 17:10:44 +0400

    Fix decimals for tokenFromDecimals

--- components/CreateOffer/Buttons/hooks/useTokenData.tsx
+++ components/CreateOffer/Buttons/hooks/useTokenData.tsx
@@ -5,13 +5,18 @@ import { useOfferCreateContext } from '@context/offer/create/OfferCreateContext'
 import { TOKEN_MAP } from '@lib/constants';
 
 export const useTokenData = () => {
-  const { offerFromState, offerToState } = useOfferCreateContext();
+  const { offerFromState, offerToState, userTokens } = useOfferCreateContext();
   const [isValid, setIsValid] = useState<boolean>(false);
 
   const data = useMemo(() => {
-    const tokenFromDecimals = TOKEN_MAP[offerFromState.from as Address]
-      ? TOKEN_MAP[offerFromState.from as Address].decimals
-      : offerFromState.decimals;
+    let tokenFromDecimals;
+    const currentToken = userTokens.tokens?.find((token) => token.address === offerFromState.from);
+    if (currentToken) {
+      tokenFromDecimals = currentToken.decimals;
+    } else {
+      tokenFromDecimals = offerFromState.decimals;
+    }
+
     const tokenToDecimals = TOKEN_MAP[offerToState.to as Address]
       ? TOKEN_MAP[offerToState.to as Address].decimals
       : offerToState.decimals;
@@ -27,7 +32,7 @@ export const useTokenData = () => {
       tokenFromDecimals,
       tokenToDecimals,
     };
-  }, [offerFromState, offerToState]);
+  }, [offerFromState.decimals, offerFromState.from, offerToState.decimals, offerToState.to, userTokens.tokens]);
 
   return {
     tokenFromAddress: offerFromState.from as Address,
